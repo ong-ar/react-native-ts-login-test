d3506e76d323c20735a79af11b675ffea2a4cfe1
ong-ar <reclusis@gmail.com>
2019-03-20 16:18:53 +0900

    kakao login test

--- App.tsx
+++ App.tsx
@@ -1,31 +1,100 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * 
- * Generated with the TypeScript template
- * https://github.com/emin93/react-native-template-typescript
- * 
- * @format
- */
+import React, { Component } from "react";
+import {
+  Platform,
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  NativeModules
+} from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+// const { RNKakaoLogins } = NativeModules;
+import RNKakaoLogins from "react-native-kakao-logins";
+const NativeButton = require("apsl-react-native-button");
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isKakaoLogging: false,
+      token: "token has not fetched"
+    };
+    if (!RNKakaoLogins) {
+      console.log("Not Linked");
+    }
+  }
 
-interface Props {}
-export default class App extends Component<Props> {
+  // 카카오 로그인 시작.
+  kakaoLogin() {
+    console.log("   kakaoLogin   ");
+    RNKakaoLogins.login((err: any, result: any) => {
+      console.log(err, result);
+      if (err) {
+        Alert.alert("error", err);
+        return;
+      }
+      Alert.alert("result", result);
+    });
+  }
+
+  kakaoLogout() {
+    console.log("   kakaoLogout   ");
+    RNKakaoLogins.logout((err: any, result: any) => {
+      if (err) {
+        Alert.alert("error", err);
+        return;
+      }
+      Alert.alert("result", result);
+    });
+  }
+
+  // 로그인 후 내 프로필 가져오기.
+  getProfile() {
+    console.log("getKakaoProfile");
+    RNKakaoLogins.getProfile((err: any, result: any) => {
+      if (err) {
+        Alert.alert("error", err);
+        return;
+      }
+      Alert.alert("result", result);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.header}>
+          <Text>LOGIN</Text>
+        </View>
+        <View style={styles.content}>
+          <NativeButton
+            isLoading={this.state.isNaverLoggingin}
+            onPress={() => this.kakaoLogin()}
+            activeOpacity={0.5}
+            style={styles.btnKakaoLogin}
+            textStyle={styles.txtNaverLogin}
+          >
+            LOGIN
+          </NativeButton>
+          <Text>{this.state.token}</Text>
+          <NativeButton
+            onPress={() => this.kakaoLogout()}
+            activeOpacity={0.5}
+            style={styles.btnKakaoLogin}
+            textStyle={styles.txtNaverLogin}
+          >
+            Logout
+          </NativeButton>
+          <NativeButton
+            isLoading={this.state.isKakaoLogging}
+            onPress={() => this.getProfile()}
+            activeOpacity={0.5}
+            style={styles.btnKakaoLogin}
+            textStyle={styles.txtNaverLogin}
+          >
+            getProfile
+          </NativeButton>
+        </View>
       </View>
     );
   }
@@ -34,18 +103,39 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: "column",
+    marginTop: Platform.OS === "ios" ? 0 : 24,
+    paddingTop: Platform.OS === "ios" ? 24 : 0,
+    backgroundColor: "white"
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  header: {
+    flex: 8.8,
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+    alignItems: "center"
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  content: {
+    flex: 87.5,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignSelf: "stretch",
+    alignItems: "center"
   },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold"
+  },
+  btnKakaoLogin: {
+    height: 48,
+    width: 240,
+    alignSelf: "center",
+    backgroundColor: "#F8E71C",
+    borderRadius: 0,
+    borderWidth: 0
+  },
+  txtNaverLogin: {
+    fontSize: 16,
+    color: "#3d3d3d"
+  }
 });
