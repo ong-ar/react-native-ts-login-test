d67555a3c95cd25a05cc2f6b82fb0856872f88d8
ong-ar <reclusis@gmail.com>
2019-03-26 17:32:18 +0900

    google login test

--- App.tsx
+++ App.tsx
@@ -13,13 +13,26 @@ import RNKakaoLogins from "react-native-kakao-logins";
 import NaverLogin from "react-native-ccs-naver-login";
 import NativeButton from "apsl-react-native-button";
 import { LoginButton, AccessToken } from "react-native-fbsdk";
+import { GoogleSignin, GoogleSigninButton, statusCodes } from 'react-native-google-signin';
+
+GoogleSignin.configure({
+  scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  webClientId: '1060901720488-52mb7d0n2ec2p2s2q7f3jsf6v5bu38vj.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+  offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  hostedDomain: '', // specifies a hosted domain restriction
+  loginHint: '', // [iOS] The user's ID, or email address, to be prefilled in the authentication UI if possible. [See docs here](https://developers.google.com/identity/sign-in/ios/api/interface_g_i_d_sign_in.html#a0a68c7504c31ab0b728432565f6e33fd)
+  forceConsentPrompt: true, // [Android] if you want to show the authorization prompt at each login.
+  accountName: '', // [Android] specifies an account name on the device that should be used
+  iosClientId: '<FROM DEVELOPER CONSOLE>', // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+});
 
 export default class App extends Component<any, any> {
   constructor(props: any) {
     super(props);
     this.state = {
       isKakaoLogging: false,
-      token: "token has not fetched"
+      token: "token has not fetched",
+      userInfo: {}
     };
     if (!RNKakaoLogins) {
       console.log("Not Linked");
@@ -62,6 +75,32 @@ export default class App extends Component<any, any> {
       })
       .catch(e => console.log(e));
   }
+  
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo, error: null });
+      console.log(userInfo);
+    } catch (error) {
+      console.log(error);
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // sign in was cancelled
+        Alert.alert('cancelled');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation in progress already
+        Alert.alert('in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        Alert.alert('play services not available or outdated');
+      } else {
+        Alert.alert('Something went wrong', error.toString());
+        this.setState({
+          error,
+        });
+      }
+    }
+  };
+
 
   render() {
     return (
@@ -111,6 +150,12 @@ export default class App extends Component<any, any> {
             }}
             onLogoutFinished={() => console.log("logout.")}
           />
+          <GoogleSigninButton
+            style={{ width: 192, height: 48 }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={this._signIn}
+            disabled={this.state.isSigninInProgress} />
         </View>
       </View>
     );
